310538292a977065bb8c2e82e0f3f6a75c1c28fb
Josecopro <juanbece2006@gmail.com>
2023-11-14 03:46:34 -0500

    prmesas funct

---
+++ src/es6/06-promises.js
@@ -0,0 +1,12 @@
+const funcion = () =>{
+    return new Promise ((resolve, reject)=>{
+        if (1<0){
+            resolve('Hello world!');
+        }else{
+            reject('Hoy estoy triste');
+        }
+    })
+} 
+funcion()
+    .then(response=>console.log(response))
+    .catch(error=>console.log(error));
