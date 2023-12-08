6bf72b71da60e985a80685ee36c3e9aa0617d24a
Josecopro <juanbece2006@gmail.com>
2023-12-07 23:57:44 -0500

    Es11 (1 class miss)

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    Josecopro: {
+        pais: "CO"
+    },
+    Yes33: {
+        pais: "CO"
+    }
+}
+console.log(users.Josecopro.pais)
+console.log(users?.Josecopro?.edad)
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const numerogrande = 928391839833482n;
+const otronumerogrande = BigInt(928391839833482n);
+console.log(numerogrande);
+console.log(otronumerogrande);
